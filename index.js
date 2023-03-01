b6c81a1de070201aef40472bd0910a9cdd9cc778
Probably Raging <probablyraging@gmail.com>
2023-03-01 19:17:08 +1100

    Finished testing

--- index.js
+++ index.js
@@ -49,10 +49,6 @@ app.use('/error', error);
 app.use('/success', success);
 app.use('/api', api);
 
-setInterval(() => {
-    console.log('retesting');
-}, 3000);
-
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 });
