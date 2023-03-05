4a2e290d0ee111f49bf41128ab84a924424e57b5
Probably Raging <probablyraging@gmail.com>
2023-03-05 21:47:27 +1100

    Testing

--- routes/api.js
+++ routes/api.js
@@ -51,7 +51,7 @@ router.get('/getusers', async (req, res) => {
 
 router.post('/addvideo', async (req, res) => {
     const origin = req.headers?.origin;
-    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV))) {
+    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV)  || origin.includes('ipjgbplbhhlbckejfoclohbngmdpgmbd'))) {
         try {
             // Fetch user data and check token count
             const userResult = await extUsers.findOne({ userId: req.body.userId });
@@ -124,7 +124,7 @@ router.post('/addvideo', async (req, res) => {
 
 router.post('/usertokens', async (req, res) => {
     const origin = req.headers?.origin;
-    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV))) {
+    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV)  || origin.includes('ipjgbplbhhlbckejfoclohbngmdpgmbd'))) {
         try {
             const userResult = await extUsers.findOne({ userId: req.body.userId });
             if (userResult) {
@@ -143,7 +143,7 @@ router.post('/usertokens', async (req, res) => {
 
 router.get('/videolist', async (req, res) => {
     const origin = req.headers?.origin;
-    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV))) {
+    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV)  || origin.includes('ipjgbplbhhlbckejfoclohbngmdpgmbd'))) {
         try {
             const results = await videoList.find().sort({ watches: 1 });
             if (results.length < 1) {
@@ -168,7 +168,7 @@ router.get('/videolist', async (req, res) => {
 
 router.post('/addtokens', async (req, res) => {
     const origin = req.headers?.origin;
-    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV))) {
+    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV)  || origin.includes('ipjgbplbhhlbckejfoclohbngmdpgmbd'))) {
         try {
             // Fetch the user and add the appropriate amount of tokens
             const userResult = await extUsers.findOne({ userId: req.body.userId });
@@ -200,7 +200,7 @@ router.post('/addtokens', async (req, res) => {
 
 router.post('/addwatch', async (req, res) => {
     const origin = req.headers?.origin;
-    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV))) {
+    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV)  || origin.includes('ipjgbplbhhlbckejfoclohbngmdpgmbd'))) {
         try {
             // Update the videos watch count
             const videoResult = await videoList.findOne({ videoId: req.body.videoId });
@@ -239,7 +239,7 @@ router.post('/addwatch', async (req, res) => {
 
 router.post('/addlike', async (req, res) => {
     const origin = req.headers?.origin;
-    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV))) {
+    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV)  || origin.includes('ipjgbplbhhlbckejfoclohbngmdpgmbd'))) {
         try {
             // Fetch the liked video's data
             const videoResult = await videoList.findOne({ videoId: req.body.videoId });
@@ -264,7 +264,7 @@ router.post('/addlike', async (req, res) => {
 
 router.post('/sendnotification', async (req, res) => {
     const origin = req.headers?.origin;
-    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV))) {
+    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV)  || origin.includes('ipjgbplbhhlbckejfoclohbngmdpgmbd'))) {
         try {
             const createChannel = await fetch(`https://discord.com/api/v10/users/@me/channels`, {
                 method: 'POST',
@@ -301,7 +301,7 @@ router.post('/sendnotification', async (req, res) => {
 
 router.post('/logout', async (req, res) => {
     const origin = req.headers?.origin;
-    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV))) {
+    if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV)  || origin.includes('ipjgbplbhhlbckejfoclohbngmdpgmbd'))) {
         try {
             extUsers.updateOne({
                 userId: req.body.userId
