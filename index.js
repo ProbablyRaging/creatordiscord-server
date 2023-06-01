c57be2257fa78ac007dbdbcf7cec2746e298282d
ProbablyRaging <probablyraging@gmail.com>
2023-06-02 03:18:26 +1000

    Use middleware to remove trailing slashes

--- index.js
+++ index.js
@@ -59,8 +59,17 @@ const api = require('./routes/api');
 app.use('/api', api);
 
 // React app route
+app.use((req, res, next) => {
+    if (req.path.substr(-1) === '/' && req.path.length > 1) {
+        const newPath = req.path.slice(0, -1);
+        const query = req.url.slice(req.path.length);
+        res.redirect(301, newPath + query);
+    } else {
+        next();
+    }
+});
+
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(slashes(false));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
