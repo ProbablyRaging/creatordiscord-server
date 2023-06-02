8b700288181d46b5f4eedac56ca1e3990c8feed1
ProbablyRaging <probablyraging@gmail.com>
2023-06-03 00:07:09 +1000

    Testing

--- routes/api.js
+++ routes/api.js
@@ -145,6 +145,7 @@ router.post('/usertokens', async (req, res) => {
 });
 
 router.get('/videolist', async (req, res) => {
+    console.log('boop');
     const origin = req.headers?.origin;
     if (origin && (origin.includes(process.env.API_KEY) || origin.includes(process.env.API_KEY_DEV) || origin.includes('ipjgbplbhhlbckejfoclohbngmdpgmbd'))) {
         try {
