31b22c1abf9a72815dd747d85c6e9c79c492df3e
ProbablyRaging <probablyraging@gmail.com>
2023-06-02 16:07:55 +1000

    Testing

--- index.js
+++ index.js
@@ -80,7 +80,8 @@ app.get('/resources', (req, res) => {
 });
 
 app.get('/resources/create', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.render(path.join(__dirname, 'dist', 'index.html'));
+    // res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.get('/resources/:slug', (req, res) => {
