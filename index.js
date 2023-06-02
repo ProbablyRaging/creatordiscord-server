062956021f489a62f0b593f6e22b7725687b41a8
ProbablyRaging <probablyraging@gmail.com>
2023-06-02 16:14:06 +1000

    Reverting

--- index.js
+++ index.js
@@ -80,8 +80,7 @@ app.get('/resources', (req, res) => {
 });
 
 app.get('/resources/create', (req, res) => {
-    res.render(path.join(__dirname, 'dist', 'index.html'));
-    // res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.get('/resources/:slug', (req, res) => {
