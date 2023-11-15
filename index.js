fba931cd7ab93b66da134f5ae1ed564704ecce15
ProbablyRaging <probablyraging@gmail.com>
2023-11-16 06:17:37 +1100

    Updated API routes for distubify

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ app.use(session({
     cookie: {
         maxAge: 60000 * 60 * 24,
         httpOnly: false,
-        domain: 'http://localhost:5173',
-        secure: true,
+        domain: 'localhost',
+        secure: false,
         sameSite: 'none',
         path: '/distubify'
     },
