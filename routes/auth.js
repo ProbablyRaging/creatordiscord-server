2abbe7d06391d7bdfc80d4c0495f123f1513497f
ProbablyRaging <probablyraging@gmail.com>
2023-11-15 15:41:31 +1100

    Updated API routes for distubify

--- routes/auth.js
+++ routes/auth.js
@@ -84,9 +84,6 @@ router.get('/success', async (req, res, next) => {
     res.cookie('distubify.rt', req.sessionID, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: false,
-        secure: true,
-        sameSite: 'None',
-        domain: 'probablyraging.dev'
     });
     await extUsers.findOneAndUpdate({ userId: req.user.id }, { sessionId: req.sessionID, });
     res.redirect('https://probablyraging.dev/distubify');
