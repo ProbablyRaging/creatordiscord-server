b231266890fe669d87cf626347f03bfdec3e3250
ProbablyRaging <probablyraging@gmail.com>
2023-11-16 05:22:20 +1100

    Updated API routes for distubify

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(session({
-    secret: 'some secret',
+    secret: 'some.secret',
     cookie: {
         maxAge: 60000 * 60 * 24
     },
